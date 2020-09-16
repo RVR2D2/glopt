8ed6527f964b57806e622d8b2e139e1dad4acceb
vadim <example@toto.by>
2020-09-17 00:42:03 +0600

    new blok 5,6,7

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const browserSync = require('browser-sync');
 const sass        = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
-// const rename = require("gulp-rename");
+const rename = require("gulp-rename");
 const imagemin = require('gulp-imagemin');
 const htmlmin = require('gulp-htmlmin');
 
@@ -21,7 +21,7 @@ gulp.task('server', function() {
 gulp.task('styles', function() {
     return gulp.src("src/sass/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        // .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest("dist/css"))
